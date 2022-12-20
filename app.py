ea40b03692f10ea83452be33cdefbefe1024bfec
krisjhamilton <krisjhamilton@users.noreply.github.com>
2022-12-20 18:46:16 +1300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import appcontext_popped
-import flask from Flask
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def hello():
     return ("Hello")
 
