7e82eb2fbc14b048978f13e2e483b8aeadf49401
krisjhamilton <krisjhamilton@users.noreply.github.com>
2022-12-20 23:08:23 +1300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import appcontext_popped
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return ("Hello")
+
+@app.route('/form_submission', methods=['POST'])
+def form_submission():
+    form_input = request.form['input_name']
+    # process form_input and return a response
+    return 'Form input received: {}'.format(form_input)
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
 
 if __name__ == '__main__':
     app.run()
