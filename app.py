7cb563dc51fc0ce6b355ed5987b1075fe4272efa
krisjhamilton <krisjhamilton@users.noreply.github.com>
2022-12-20 23:41:48 +1300

    update

--- app.py
+++ app.py
@@ -1,7 +1,57 @@
-from flask import appcontext_popped
+# from flask import appcontext_popped
 from flask import Flask, request, render_template
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+print "Opened database successfully";
+
+conn.execute('CREATE TABLE athletes (name TEXT, age TEXT, division TEXT)')
+print "Table created successfully";
+conn.close()
 
 app = Flask(__name__)
+
+@app.route('/enternew')
+def new_student():
+   return render_template('student.html')
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+   if request.method == 'POST':
+      try:
+         nm = request.form['name']
+         age = request.form['age']
+         division = request.form['division']
+         
+         with sql.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO students (name,age,division) 
+               VALUES (?,?,?,?)",(nm,addr,city,pin) )
+            
+            con.commit()
+            msg = "Record successfully added"
+      except:
+         con.rollback()
+         msg = "error in insert operation"
+      
+      finally:
+         return render_template("result.html",msg = msg)
+         con.close()
+
+
+@app.route('/list')
+def list():
+   con = sql.connect("database.db")
+   con.row_factory = sql.Row
+   
+   cur = con.cursor()
+   cur.execute("select * from students")
+   
+   rows = cur.fetchall(); 
+   return render_template("list.html",rows = rows)
+
+
+
 
 
 @app.route('/')
